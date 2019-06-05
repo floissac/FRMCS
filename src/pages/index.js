b6a5bf0974d979fe00567d685aaea9c6b2c0e806
floissac <floissac.w@gmail.com>
2019-06-05 17:26:41 +0100

    added styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Menu from 'components/Menu';
+import '../styles/base.scss';
 
 ReactDOM.render(<Menu />, document.getElementById('menu'));
